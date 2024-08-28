b298c164fc8655438e2aa9d5ff9dba3e4936aff9
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2024-08-28 08:17:46 +0200

    add vercel database connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,5 @@
 // lib/db.js
-const { Pool } = require("pg");
+import { createPool } from "@vercel/postgres";
 
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
-});
-
+const pool = createPool({});
 export default pool;
