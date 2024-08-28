e7af1bfb0f7dc0de011ff9a57e1c1d6994fb986a
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2024-08-28 08:47:24 +0200

    add vercel backend connection

--- utils/utils.ts
+++ utils/utils.ts
@@ -19,7 +19,10 @@ export async function loadSpending(date: string) {
 }
 
 async function getData(date: string) {
-  const res = await fetch(`http://localhost:5555/api/date/${date}`);
+  console.log(process.env.NEXT_PUBLIC_BACKEND_URL, "hhhhhhhhh");
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}api/date/${date}`,
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -29,29 +32,38 @@ async function getData(date: string) {
 
 export async function deleteSpending(id: number) {
   try {
-    const res = await fetch(`http://localhost:5555/api/spending/${id}`, {
-      method: "DELETE",
-    });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}api/spending/${id}`,
+      {
+        method: "DELETE",
+      },
+    );
   } catch (err) {
     console.log(err);
   }
 }
 
 export async function addSpending(spent: Partial<Spent>) {
-  const res = await fetch(`http://localhost:5555/api/spending/`, {
-    method: "POST",
-    body: JSON.stringify(spent),
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}api/spending/`,
+    {
+      method: "POST",
+      body: JSON.stringify(spent),
+    },
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 }
 
 export async function editSpending(spent: Spent) {
-  const res = await fetch(`http://localhost:5555/api/spending/`, {
-    method: "PUT",
-    body: JSON.stringify(spent),
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}api/spending/`,
+    {
+      method: "PUT",
+      body: JSON.stringify(spent),
+    },
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -59,7 +71,9 @@ export async function editSpending(spent: Spent) {
 
 export async function getYearSpending(year: string) {
   try {
-    const res = await fetch(`http://localhost:5555/api/spendings/${year}`);
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}api/spendings/${year}`,
+    );
     console.log(res);
     return (await res.json()) as Spent[];
   } catch (error) {
